fade1033ac168c83e818236de54c64095988b3dc
Rodrigo Knob <r_knob@outlook.com>
2022-04-07 20:03:12 -0300

    Project created

---
+++ src/test/java/com/rodrigo/coursemc/CoursemcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodrigo.coursemc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursemcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
